fd30cdc647a8764fbd42936488ee4b2ada7a1ade
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-23 01:18:11 -0700

    3-main

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
+say_my_name("Haile", "Abie")
+say_my_name("Alex", "Abie")
+say_my_name("Habtu")
 try:
-    say_my_name(12, "White")
+    say_my_name(15, "Abie")
 except Exception as e:
     print(e)
