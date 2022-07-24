4f005fa2a17850d4b9d10d166b5d58f7e0f91c9f
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-24 02:23:20 -0700

    5-main

---
+++ 0x07-python-test_driven_development/5-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""With ALX’s best-in-class training, and our commitment to lifelong learning, we’ve created an unrivaled opportunity to prepare, launch and grow your career."")
+print()
+text_indentation("ALX Student? How are you:   Hailemariam")
