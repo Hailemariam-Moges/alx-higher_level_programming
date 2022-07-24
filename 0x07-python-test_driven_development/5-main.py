beff1f8e4c28f10a29496a103d4c8b4420b0eba1
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-24 02:28:48 -0700

    5-main

--- 0x07-python-test_driven_development/5-main.py
+++ 0x07-python-test_driven_development/5-main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 text_indentation = __import__('5-text_indentation').text_indentation
 
-text_indentation("""With ALX’s best-in-class training, and our commitment to lifelong learning, we’ve created an unrivaled opportunity to prepare, launch and grow your career."")
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
 print()
 text_indentation("ALX Student? How are you:   Hailemariam")
